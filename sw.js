616e4a57dc4de5f36987323223cd9347777bfe06
L.P <lucaspombal9@gmail.com>
2026-05-12 03:56:58 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,12 @@
-const CACHE_NAME = 'sleep-pro-v1';
-const ASSETS = ['./', './index.html', './style.css', './app.js', './manifest.json'];
-
-self.addEventListener('install', (event) => {
-    event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+self.addEventListener('install', (e) => {
+    e.waitUntil(caches.open('sleep-pro-v1').then(c => c.addAll(['./', './index.html', './app.js', './style.css'])));
 });
 
-self.addEventListener('fetch', (event) => {
-    event.respondWith(caches.match(event.request).then(res => res || fetch(event.request)));
+self.addEventListener('fetch', (e) => {
+    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
 
-// Evento ao clicar na notificação
-self.addEventListener('notificationclick', (event) => {
-    event.notification.close();
-    event.waitUntil(clients.openWindow('./index.html'));
-});
-
-// Evento de push (se futuramente você usar servidor)
-self.addEventListener('push', (event) => {
-    const data = event.data.json();
-    self.registration.showNotification(data.title, {
-        body: data.body,
-        icon: './sleep-football-pro-192x192.png'
-    });
+self.addEventListener('notificationclick', (e) => {
+    e.notification.close();
+    e.waitUntil(clients.openWindow('./index.html'));
 });
